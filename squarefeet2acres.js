4086880ee48323d34ccbbc8e9484fd7471ca21b6
MCapredoni <144966520+MCapredoni@users.noreply.github.com>
2025-03-04 23:22:28 +0000

    Test 1 Complete

--- squarefeet2acres.js
+++ squarefeet2acres.js
@@ -1,15 +1,6 @@
-// This program *converts* **squarefeet** to **acres**
+function squarefeet2acres(squarefeet){
+    let acres = squarefeet/43560;
+    return acres;
+}
 
-let squarefeet = 350000;
-
-let acres = squarefeet/43560;
-
-if (squarefeet >= 100000){
-    acres = "2.295684113865932";
-}   else if (squarefeet >= 125000){
-    acres = "2.869605142332415";
-}   else if (squarefeet >= 350000){
-    acres = "8.034894398530762";
-}   
-
-console.log(`${squarefeet} squarefeet is ${acres} acres`);
+export {squarefeet2acres}
