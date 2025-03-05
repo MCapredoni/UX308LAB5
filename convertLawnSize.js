338ac6eff59d643a83406cf67537d3a9a2a1f73a
MCapredoni <144966520+MCapredoni@users.noreply.github.com>
2025-03-05 02:30:27 +0000

    gave up

---
+++ convertLawnSize.js
@@ -0,0 +1,8 @@
+// Calculate the **minutes** of lawn mowing. In **metres** start with a variable that references the **length**, and another for **width**, and another for **squaremetres**.
+
+function convertLawnSize(length, width, metres){
+    let metres = length * width;
+    return minutes;
+}
+
+export {convertLawnSize}
---
+++ convertLineSlope.js
@@ -0,0 +1,8 @@
+// Calculate the **slope** of a line. Variables are **rise** and **run**.
+
+function convertLineSlope (x1, y1, x2, y2){
+    if(slope = x1, y1, x2, y2);
+    return (y2 - y1) / (x2 - x1)
+}
+
+export {convertLineSlope}
